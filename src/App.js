f16054bd1c3a53a67e087d047e8e234f88892240
Alex Via <alexandervia@gmail.com>
2022-12-07 11:25:52 -0600

    Hello world

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import React from 'react';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-export default class App extends React.Component {
-  render() {
-      return (
-          <h1>{this.props.type} component</h1>
-      )
-  }
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1>Hello World</h1>
+                <ul>
+                    <li>This is coding with React</li>
+                    <li>Learning through Scrimba</li>
+                    <li>Using v 18 locally</li>
+                </ul>
+            </header>
+        </div>
+    );
 }
 
-// export default App;
+export default App;
